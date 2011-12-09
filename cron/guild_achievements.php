17be29dca79da5c11099727f6938fd75cde3ca8e
Cal Henderson <cal@iamcal.com>
2011-12-09 15:24:52 -0600

    switched to using battlenet api for grabbing achievements

--- cron/guild_achievements.php
+++ cron/guild_achievements.php
@@ -7,59 +7,64 @@
 
 	include(dirname(__FILE__).'/../include/init.php');
 
-	loadlib('xml');
-	loadlib('curl');
+	loadlib('bnet');
 
 	#######################################################################################################
 
 	#
-	# grab categories xml
+	# grab possible achievements
 	#
 
-	echo "Categories...";
+	echo "Catalog...";
 
-	$tree = fetch_safe("http://www.wowarmory.com/character-achievements.xml?r=Hyjal&cn=Bees&gn=The+Eternal");
-	$cat_nodes = $tree->findMulti('page/achievements/rootCategories/category');
-	$tree->cleanup();
+	$ret = bnet_fetch_safe('us', '/data/character/achievements', 10);
+	if (!$ret['ok']){
+		print_r($ret);
+		exit;
+	}
 
+	$groups = array();
+	$catalog = array();
 
-	#
-	# flatten
-	#
+	foreach ($ret['data']['achievements'] as $group){
 
-	$cats = array();
-	$major_cat_ids = array();
-	$c_num = 0;
+		$groups[] = array($group['id'], 0, $group['name']);
 
-	foreach ($cat_nodes as $node){
+		if (is_array($group['achievements']))
+		foreach ($group['achievements'] as $row){
+			$row['group_id'] = $group['id'];
+			$row['num_players'] = 0;
+			$catalog[$row['id']] = $row;
+		}
 
-		$c_num++;
+		if (is_array($group['categories']))
+		foreach ($group['categories'] as $cat){
+			$groups[] = array($group['id'], $cat['id'], $cat['name']);
 
-		$cats[] = array($node->attributes[id], 0, $node->attributes[name], $c_num);
-		$major_cat_ids[] = $node->attributes[id];
-		$num = 0;
+			foreach ($cat['achievements'] as $row){
 
-		foreach ($node->children as $child){
-
-			if (!$child->name == 'category') continue;
-			$num++;
-
-			$cats[] = array($node->attributes[id], $num, $child->attributes[name], $c_num);
+				$row['group_id'] = $group['id'];
+				$row['cat_id'] = $cat['id'];
+				$row['num_players'] = 0;
+				$catalog[$row['id']] = $row;
+			}
 		}
 	}
 
 
 	#
-	# update keys table
+	# update cats table
 	#
 
+	$c = 0;
 	db_query("DELETE FROM guild_achievements_cats");
-	foreach ($cats as $cat){
+	foreach ($groups as $row){
+		$c++;
 		db_insert('guild_achievements_cats', array(
-			'id'		=> intval($cat[0]),
-			'sub_id'	=> intval($cat[1]),
-			'in_order'	=> intval($cat[3]),
-			'name'		=> AddSlashes($cat[2]),
+			'id'		=> intval($row[0]),
+			'sub_id'	=> intval($row[1]),
+			'in_order'	=> intval($c),
+			'name'		=> AddSlashes($row[2]),
 		));
 	}
 
@@ -71,26 +76,30 @@
 
 	$players = array();
 
-	$tree = fetch_safe("http://www.wowarmory.com/guild-info.xml?r=Hyjal&cn=Bees&gn=The+Eternal");
-	$player_nodes = $tree->findMulti('page/guildInfo/guild/members/character');
-	foreach ($player_nodes as $node){
-
-		$players[] = $node->attributes[name];
+	$ret = bnet_fetch_safe('us', '/guild/hyjal/the%20eternal?fields=members', 1);
+	if (!$ret['ok']){
+		print_r($ret);
+		exit;
 	}
 
-	$tree->cleanup();
+	foreach ($ret['data']['members'] as $member){
+
+		$players[] = $member['character']['name'];
+	}
+
 	echo "ok\n";
+
 
 	#######################################################################################################
 
-	$catalog = array();
-	$hashes = array();
+	echo "Achievements...";
 
+	$hashes = array();
 	#$players = array_slice($players, 0, 5);
 
 	foreach ($players as $player){
 
-		$stats = fetch_player($player, $catalog);
+		$stats = fetch_player($player);
 
 		foreach ($stats as $id => $ts){
 			$hashes[] = array(
@@ -98,32 +107,40 @@
 				'player' => AddSlashes($player),
 				'when' => intval($ts),
 			);
+			$catalog[$id]['num_players']++;
 		}
 	}
 
 	db_query("DELETE FROM guild_achievements_data");
 	db_insert_multi('guild_achievements_data', $hashes, 1000);
 
+	echo "ok\n";
+
 	#######################################################################################################
 
-	echo "Catalog...";
+	echo "Updating catalog...";
+
+	#
+	# store the catalog
+	#
 
 	db_query("DELETE FROM guild_achievements_key");
 
 	$hashes = array();
-	foreach ($catalog as $k => $row){
+	foreach ($catalog as $row){
 		$hashes[] = array(
-			'id'		=> intval($k),
-			'cat_id'	=> intval($row[cat_id]),
-			'sub_id'	=> intval($row[sub_id]),
-			'title'		=> AddSlashes($row[title]),
-			'desc'		=> AddSlashes($row[desc]),
-			'icon'		=> AddSlashes($row[icon]),
-			'points'	=> intval($row[points]),
-			'num_players'	=> intval($row[num_players]),
+			'id'		=> intval($row['id']),
+			'cat_id'	=> intval($row['group_id']),
+			'sub_id'	=> intval($row['cat_id']),
+			'title'		=> AddSlashes($row['title']),
+			'desc'		=> AddSlashes($row['description']),
+			'icon'		=> AddSlashes($row['icon']),
+			'points'	=> intval($row['points']),
+			'num_players'	=> intval($row['num_players']),
 		);
 	}
 	db_insert_multi('guild_achievements_key', $hashes);
+
 
 	echo "ok\n";
 
@@ -131,81 +148,25 @@
 	#######################################################################################################
 	#######################################################################################################
 
-	function fetch_safe($url){
+	function fetch_player($player){
 
-		while (1){
-			$tree = xml_fetch($url);
-			if ($tree) return $tree;
-			echo '.'; flush();
-			sleep(1);
-		}
-	}
+		$name_stub = str_replace("%27", "'", rawurlencode($player));
+		$url = "/character/hyjal/{$name_stub}?fields=achievements";
 
-	#######################################################################################################
-
-	function fetch_player($player, &$catalog){
-
-		global $major_cat_ids;
-
-		$player_url = urlencode($player);
-		$stats = array();
-
-		echo "$player... ";
-
-		foreach ($major_cat_ids as $cat_id){
-
-			echo "$cat_id ";
-
-			$tree = fetch_safe("http://www.wowarmory.com/character-achievements.xml?r=Hyjal&n={$player_url}&c={$cat_id}");
-
-			$a_nodes = $tree->findMulti('achievements/category/achievement');
-			spider_nodes($stats, $catalog, $cat_id, 0, $a_nodes);
-
-			$num = 0;
-
-			$c_nodes = $tree->findMulti('achievements/category/category');
-			foreach ($c_nodes as $c_node){
-
-				$num++;
-
-				$a_nodes = $c_node->findMulti('achievement');
-				spider_nodes($stats, $catalog, $cat_id, $num, $a_nodes);
-			}
-
-			$tree->cleanup();
+		$ret = bnet_fetch_safe('us', $url, 1);
+		if (!$ret['ok']){
+			if ($ret['req']['status'] == 404) return array();
+			print_r($ret);
+			exit;
 		}
 
-		echo "ok\n";
+		$out = array();
 
-		return $stats;
-	}
+		foreach ($ret['data']['achievements']['achievementsCompleted'] as $k => $v){
 
-	#######################################################################################################
-
-	function spider_nodes(&$stats, &$catalog, $cat_id, $sub_id, $nodes){
-
-		foreach ($nodes as $node){
-
-			# this picks up nested achievements
-			$sub_nodes = $node->findMulti('achievement');
-			spider_nodes($stats, $catalog, $cat_id, $sub_id, $sub_nodes);
-
-			$id = $node->attributes[id];
-
-			if ($node->attributes[dateCompleted]){
-				$when = strtotime($node->attributes[dateCompleted]);
-				$stats[$id] = $when;
-				$catalog[$id]['num_players']++;
-			}
-
-			$catalog[$id]['cat_id']	= $cat_id;
-			$catalog[$id]['sub_id']	= $sub_id;
-			$catalog[$id]['title']	= $node->attributes[title];
-			$catalog[$id]['desc']	= $node->attributes[desc];
-			$catalog[$id]['icon']	= $node->attributes[icon];
-			$catalog[$id]['points']	= $node->attributes[points];
+			$out[$v] = substr($ret['data']['achievements']['achievementsCompletedTimestamp'][$k], 0, -3);
 		}
-	}
 
-	#######################################################################################################
+		return $out;
+	}
 ?>
